5af4a46b18c768614eda10cc578eda1cb41f5f54
GeorgeNicola <nicola_george1999@yahoo.com>
2020-08-20 21:26:39 +0300

    Database connection 2

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const image = require('./controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-        host: process.env.DATABASE_URL,
+        connectionString: process.env.DATABASE_URL,
         ssl: true,
     /*  host : '127.0.0.1',   // 127.0.0.1 = localhost
       user : 'postgres',
